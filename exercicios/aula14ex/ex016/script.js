da1b95bbc4c7506b46557f013983d3f68ed89e83
João Pellisoli <pellisolij@gmail.com>
2021-09-15 01:49:03 -0300

    add  pasta aula14 e pasta aula14ex

---
+++ exercicios/aula14ex/ex016/script.js
@@ -0,0 +1,9 @@
+function verificar() {
+    var inicio = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var passo = document.getElementById('passo')
+    var res = document.getElementById('res')
+    for (inicio; fim; c++) {
+        console.log(inicio)
+    }
+}
